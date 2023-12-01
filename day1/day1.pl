7fc6ec233c7bc1912b31d2242135c1aeca702143
Michael Tokar <mtokar@atlassian.com>
2023-12-02 01:14:57 +1100

    Validated Part 2 answer and mass refactoring

--- day1/day1.pl
+++ day1/day1.pl
@@ -13,40 +13,48 @@ use warnings;
 my $input_string = <STDIN>;
 chomp($input_string);
 
-my $word_pattern = qr/^(one|two|three|five|seven|eight|nine)/;
+my $word_pattern = qr/^(one|two|three|four|five|six|seven|eight|nine)/;
+my $digit_or_word = qr/^[1-9]|^(one|two|three|four|five|six|seven|eight|nine)/;
 
+# replace the start of the string word with a digit, keep the rest
+# we must keep the last letter of the word to enable chains of words to be parsed correctly
 sub replace_word {
 	my ($word) = @_;
-	$word =~ s/^one(.*)/1$1/;
-	$word =~ s/^two(.*)/2$1/;
-	$word =~ s/^three(.*)/3$1/;
-	$word =~ s/^four(.*)/4$1/;
-	$word =~ s/^five(.*)/5$1/;
-	$word =~ s/^six(.*)/6$1/;
-	$word =~ s/^seven(.*)/7$1/;
-	$word =~ s/^eight(.*)/8$1/;
-	$word =~ s/^nine(.*)/9$1/;
+	$word =~ s/^one(.*)/1e$1/;
+	$word =~ s/^two(.*)/2o$1/;
+	$word =~ s/^three(.*)/3e$1/;
+	$word =~ s/^four(.*)/4r$1/;
+	$word =~ s/^five(.*)/5e$1/;
+	$word =~ s/^six(.*)/6x$1/;
+	$word =~ s/^seven(.*)/7n$1/;
+	$word =~ s/^eight(.*)/8t$1/;
+	$word =~ s/^nine(.*)/9e$1/;
 	return $word;
 }
 
-my $digit_or_word = qr/^[1-9]|^(one|two|three|four|five|six|seven|eight|nine)/;
+sub behead {
+	my ($str) = @_;
+	return (substr($str, 0, 1), substr($str, 1));
+}
+
 
 # keep chopping off characters until we get a digit or word to start the string
 #print "$input_string\n";
 while ($input_string !~ $digit_or_word) {
-	$input_string = substr($input_string, 1);
+	($_, $input_string) = behead($input_string);
 #	print "$input_string\n";
 }
 
+# if leading with word then replace with digit
 if ($input_string =~ $word_pattern) {
 	$input_string = replace_word($input_string);
 }
-my $result1 = substr($input_string, 0, 1);
+
+# store our first digit 
+my $result1 = "";
+($result1, $input_string) = behead($input_string);
 #print "Found first: $result1\n";
 my $result2 = $result1;
-
-# truncate and keep going
-$input_string = substr($input_string, 1);
 
 # as we encounter new words we replace them with digits
 while (length($input_string) > 0) {
@@ -56,10 +64,10 @@ while (length($input_string) > 0) {
 #			print "$input_string matches word\n";
 			$input_string = replace_word($input_string);
 		}
-		$result2 = substr($input_string, 0, 1);
+		($result2, $_) = behead($input_string);
 #		print "Storing new result2 as $result2\n";
 	}
-	$input_string = substr($input_string, 1);
+	($_, $input_string) = behead($input_string);
 }
 
 print "$result1$result2";
--- day1/day1.sh
+++ day1/day1.sh
@@ -1,98 +1,13 @@
 #!/bin/zsh
 
-strip_middle() {
-  local line="$1"
-
-  # while length of line is greater than 2, remove everything except first and last digit
-  ml_length=`echo -n "$line" | wc -c`
-  while [ $ml_length -gt 2 ]; do
-    line=`echo "$line" | sed -E -e 's/^([0-9]).*([0-9])$/\1\2/'`
-    ml_length=`echo -n "$line" | wc -c`
-  done
-
-  # for a single digit line, repeat the digit
-  while [ $ml_length -lt 2 ]; do
-    line=`echo "$line" | sed -E -e 's/^([0-9])/\1\1/'`
-    ml_length=`echo -n "$line" | wc -c`
-  done
-
-  echo "$line"
-}
-
 # Function to process each line using regular expressions
 process_line() {
   local line="$1"
   # variable for previous number, to be added to current line
   local previous="$2"
 
-  if [[ "$line" =~ ^[1-9] ]]; then
-    if [[ "$line" =~ [1-9]$ ]]; then
-        line=$(strip_middle "$line")
-
-        # add the current line to previous and 'return' it
-        echo "$((line + previous))"
-        return
-    fi
-  fi
-
-  # four and six are safe so just swap them now
-  line=`echo "$line" | sed -E -e 's/four/4/g'`
-  line=`echo "$line" | sed -E -e 's/six/6/g'`
-
-  # process line char by char and look for either a digit or a word
-  while [[ ! "$line" =~ ^[1-9]|one|two|three|five|seven|eight|nine ]]; do
-    line=`echo "$line" | sed -E -e 's/^.*/4/g'`
-  done
-
-  # change spelled out digits into actual digits
-  # get rid of all characters not in a digit
-  #modified_line=`echo "$line" | sed -E -e 's/[^onetwothreefourfivesixseveneightnine123456789]//g'`
-
-  # have to figure out how to deal with a case like `eightwofour` = 84 not 24
-  # eighthreeight = 88 not 33
-  # words that are safe: four; six
-  modified_line=`echo "$line" | sed -E -e 's/four/4/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/six/6/g'`
-
-  # one two three five seven eight nine
-  # oneight => 1
-  # twone => 2
-  modified_line=`echo "$modified_line" | sed -E -e 's/oneight/1ight/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/twone/2ne/g'`
-
-  # eightwo => 8
-  modified_line=`echo "$modified_line" | sed -E -e 's/eightwo/8wo/g'`
-
-  # threeight => 3
-  # eighthree => 8
-  modified_line=`echo "$modified_line" | sed -E -e 's/threeight/3ight/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/eighthree/8hree/g'`
-
-  # fiveight => 5
-  modified_line=`echo "$modified_line" | sed -E -e 's/fiveight/5ight/g'`
-
-  # sevenine => 7
-  modified_line=`echo "$modified_line" | sed -E -e 's/sevenine/7ine/g'`
-
-  # nineight => 9
-  modified_line=`echo "$modified_line" | sed -E -e 's/nineight/9ight/g'`
-
-  # the rest
-  modified_line=`echo "$modified_line" | sed -E -e 's/one/1/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/two/2/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/three/3/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/five/5/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/seven/7/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/eight/8/g'`
-  modified_line=`echo "$modified_line" | sed -E -e 's/nine/9/g'`
-
-  # remove every non-digit from start of line to first digit
-  modified_line=`echo "$modified_line" | sed -E -e 's/^[^0-9]*([0-9])/\1/'`
-
-  # remove every non-digit from last digit to end of line
-  modified_line=`echo "$modified_line" | sed -E -e 's/[^0-9]*([0-9])[^0-9]*$/\1/'`
-
-  modified_line=$(strip_middle "$modified_line")
+  # use Perl instead
+  modified_line=`echo $line | ./day1.pl`
 
   # add the current line to previous and 'return' it
   echo "$((modified_line + previous))"
